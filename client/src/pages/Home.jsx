ea641f7e56a13dd4abbd99cdb32585ca77e24d52
Yoshita09 <yoshitasinghal.2005@gmail.com>
2025-11-07 07:13:44 +0530

    Navbar created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
