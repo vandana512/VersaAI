541f255f25334ffbd31782fe741376144b3d519e
Sadhna Kumari <ss.sadhna263@gmail.com>
2025-11-08 22:36:37 +0530

    Gradient fixed

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='px-4 sm:px-20 xl:px-32 relative inline flex flex-col w-full 
-        justify center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen '>
+        <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full 
+        justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
 
         </div>
     )
