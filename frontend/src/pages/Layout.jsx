878cb4fb5ae37b33076de8d2cc40ecf0adf8aa37
Vandana <vandanaaa512@gmail.com>
2025-11-09 02:15:32 +0530

    Layout - Navbar created

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import {assets} from '../assets/assets.js';
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+
+  const navigate = useNavigate()
+  const [sidebar, setSidebar]= useState(false)
+
   return (
-    <div>
-      <h1>Layout</h1>
+    <div className='flex flex-col items-start justify-start h-screen'>
+      
+      <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
+        <img src={assets.logo} alt="" onClick={()=>navigate('/')} />
+        {
+          sidebar ? <X onClick={()=>setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'/>
+          : <Menu onClick={()=>setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
+        }
+
+      </nav>
       <Outlet />
     </div>
   );
