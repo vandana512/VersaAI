c227797e1c4a2e0b6d48509fe6a663f665a44d9c
Yoshita09 <yoshitasinghal.2005@gmail.com>
2025-11-13 20:52:09 +0530

    Dashboard completed [fully working]

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,33 @@
 import { useEffect, useState } from "react";
 import { Gem, Sparkles } from "lucide-react";
-import { Protect } from "@clerk/clerk-react";
+import { Protect, useAuth } from "@clerk/clerk-react";
 import { dummyCreationData } from '../assets/assets'
 import CreationItem from "../components/CreationItem";
+import axios from "axios";
+import toast from "react-hot-toast";
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
+  const [loading, setLoading] = useState(true);  
+  const { getToken } = useAuth();
 
   const getDashboardData = async () => {
-    setCreations(dummyCreationData);
+    try {
+      const {data}= await axios.get('/api/user/get-user-creations',{
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      })
+
+      if (data.success) {
+        setCreations(data.creations);
+      }else{
+        toast.error(data.creations)
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -48,13 +66,24 @@ const Dashboard = () => {
         </div>
       </div>
 
-    
-        <div className="space-y-3">
+      {
+        loading ? (
+          <div className="flex justify-center items-center h-3/4">
+            <div className="animate-spin rounded-full h-11 w-11 border-3 border-purple-500 border-t-transparent"></div>
+          </div>
+        )
+        :
+        (
+          <div className="space-y-3">
           <p className="mt-6 mb-4">Recent Creations</p>
+          
           {creations.map((item) => (
             <CreationItem key={item.id} item={item} />
           ))}
         </div>
+        )
+      }
+        
    
     </div>
   );
